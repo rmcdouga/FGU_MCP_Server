295a7388f77ca2259b5c5da77906def4a6cbff5c
Rob McDougall <rmcdougawww@rogers.com>
2026-08-15 15:32:30 -0400

    🎉 Initial MCP Project
    From start.spring.io.

---
+++ Java/fg_mcp_server/src/main/java/io/github/rmcdouga/fgumcpserver/FguMcpServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.rmcdouga.fgumcpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FguMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FguMcpServerApplication.class, args);
+	}
+
+}
---
+++ Java/fg_mcp_server/src/test/java/io/github/rmcdouga/fgumcpserver/FguMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rmcdouga.fgumcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FguMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
