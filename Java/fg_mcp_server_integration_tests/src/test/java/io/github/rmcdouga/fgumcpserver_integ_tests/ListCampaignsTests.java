be05254fc7459ddf146537282e8b7760889ca268
Rob McDougall <rmcdougawww@rogers.com>
2026-09-22 09:02:03 -0400

    Updated test to reflect my current machine's configuration.
    Will update the test to use temp directory in the near future.

--- Java/fg_mcp_server_integration_tests/src/test/java/io/github/rmcdouga/fgumcpserver_integ_tests/ListCampaignsTests.java
+++ Java/fg_mcp_server_integration_tests/src/test/java/io/github/rmcdouga/fgumcpserver_integ_tests/ListCampaignsTests.java
@@ -1,15 +1,12 @@
 package io.github.rmcdouga.fgumcpserver_integ_tests;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled("This test is not working yet, but eventually it should be possible to get a list of campaigns from the server.")
 @SpringBootTest
 class ListCampaignsTests {
 
@@ -22,6 +19,6 @@ class ListCampaignsTests {
    				.content();
 	   	
 	   	System.out.println("Available campaigns: " + result);
-   	   	assertThat(result).contains("listFguCampaigns");		
+   	   	assertThat(result).contains("Tutorial 5E Campaign", "SimpleTestCampaign");		
 	}
 }
